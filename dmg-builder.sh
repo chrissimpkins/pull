60b5567f6e575ce458f3f9743d27d138d5a503cf
Chris Simpkins <git.simpkins@gmail.com>
2016-04-01 15:15:01 -0400

    added dmg builder script and installer image

---
+++ dmg-builder.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+/Users/ces/Desktop/extcode/create-dmg/create-dmg \
+--volname "Pull Installer" \
+--volicon "/Users/ces/Library/Application Support/Platypus/PlatypusIcon-35919.icns" \
+--background "img/dmg-installer-bg.png" \
+--window-pos 200 120 \
+--window-size 800 400 \
+--icon-size 100 \
+--icon Pull.app 200 190 \
+--hide-extension Pull.app \
+--app-drop-link 600 185 \
+installer/Pull-Installer.dmg \
+/Users/ces/Desktop/code/pull/build
